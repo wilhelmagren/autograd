19f96255c2885fb06e79a7ba09c73b3dda476d61
Eli Vance <wagren@kth.se>
2022-03-08 23:31:05 +0100

    Adam import

--- autograd/__init__.py
+++ autograd/__init__.py
@@ -1,6 +1,6 @@
 from .tensor import Tensor
 from .ops import *
 from .datautil import fetch_mnist
-from .optim import SGD
+from .optim import SGD, Adam
 from .nn import NN
 from .criterion import NLLLoss
