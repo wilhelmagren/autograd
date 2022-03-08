acd7f3941bbea4e99af1f876fee19a3a458fed2b
Eli Vance <wagren@kth.se>
2022-03-09 00:21:22 +0100

    please work...

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='autograd',
-        version='0.1.0',
-        author='Wilhelm Ågren',
-        install_requires=['numpy', 'requests', 'torch'],
-        python_requires='>=3.8',
-        include_package_data=True)
