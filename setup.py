5e4722ff8d292b4842d53f2055a6207939fae78f
Eli Vance <wagren@kth.se>
2022-03-09 00:07:04 +0100

    remove test stuff from setup

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(name='autograd',
         version='0.1.0',
         author='Wilhelm Ågren',
-        install_requires=['numpy', 'requests'],
+        install_requires=['numpy', 'requests', 'torch'],
         python_requires='>=3.8',
-        extras_requires={'testing':['torch','tqdm']}, 
         include_package_data=True)
