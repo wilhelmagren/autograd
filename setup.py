11703b37a90a4484b945a71f3243f5d9d787510b
Eli Vance <wagren@kth.se>
2022-03-09 00:03:19 +0100

    syntax error fix

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(name='autograd',
         version='0.1.0',
         author='Wilhelm Ågren',
         install_requires=['numpy', 'requests'],
-        python_requires='>=3.8'
+        python_requires='>=3.8',
         extras_requires={'testing':['torch','tqdm'], 
         include_package_data=True)
