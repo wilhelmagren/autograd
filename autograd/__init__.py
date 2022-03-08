dfde851d970f2c701939e0ffabe95f763707cc8a
Eli Vance <wagren@kth.se>
2022-03-08 16:45:39 +0100

    init add stuff

--- autograd/__init__.py
+++ autograd/__init__.py
@@ -1,5 +1,6 @@
 from .tensor import Tensor
 from .ops import *
-from .datautil import *
+from .datautil import fetch_mnist
 from .optim import SGD
 from .nn import NN
+from .criterion import NLLLoss
