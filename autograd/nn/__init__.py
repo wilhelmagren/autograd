50488d44b18c75334681432b75c3a34f5eba3d9c
ulysses@bund-subsystem <wagren@kth.se>
2022-03-12 17:07:36 +0100

    #15 add LogSoftmax nn.Module

--- autograd/nn/__init__.py
+++ autograd/nn/__init__.py
@@ -1,7 +1,8 @@
 from .nn import Module, Sequential
 from .dense import Dense
 from .relu import ReLU
+from .logsoftmax import LogSoftmax
 
 __all__ = [
-    Module, Sequential, Dense, ReLU
+    Module, Sequential, Dense, ReLU, LogSoftmax
 ]
