cd1c9f581994a39d4723b9d1d0d8eb74f4f49fc2
Eli Vance <wagren@kth.se>
2022-04-20 17:11:47 +0200

    #22 remove in-place ReLU, TODO: implement axis for logsoftmax

--- autograd/nn/activations.py
+++ autograd/nn/activations.py
@@ -3,14 +3,7 @@ from autograd.nn import Module
 
 
 class ReLU(Module):
-    def __init__(self, inplace=False):
-        self.inplace_ = inplace
-
     def forward(self, x):
-        if self.inplace_:
-            raise NotImplementedError(
-            f'still have not figured out how to do this with nn.Sequential model')
-
         return x.relu()
 
 
